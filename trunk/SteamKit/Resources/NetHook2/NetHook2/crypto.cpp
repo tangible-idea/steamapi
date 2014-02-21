1af80249f95029ac4db8fb940b5950eba23b716e
thks10 <thks10@1507e5fa-ecf2-0410-b220-ea1f1695357d>
2014-02-21 16:31:26 +0000

    steam app prototype commit.

---
+++ trunk/SteamKit/Resources/NetHook2/NetHook2/crypto.cpp
@@ -0,0 +1,199 @@
+
+
+#include "crypto.h"
+
+#include "logger.h"
+#include "csimplescan.h"
+
+#include <cassert>
+
+#include <map>
+
+
+
+bool (__cdecl *Encrypt_Orig)(const uint8*, uint32, const uint8*, uint32, uint8*, uint32*, const uint8*, uint32) = 0;
+bool (__cdecl *Decrypt_Orig)(const uint8*, uint32, uint8*, uint32*, const uint8*, uint32) = 0;
+bool (__cdecl *GetMessageFn)( int * ) = 0;
+
+
+
+struct MsgInfo_t
+{
+	EMsg eMsg;
+	const char *pchMsgName;
+	int nFlags;
+	EServerType k_EServerTarget;
+
+	uint32 nTimesSent;
+	uint64 uBytesSent;
+	
+	uint32 nTimesSentProfile;
+	uint64 uBytesSentProfile;
+
+	uint64 uUnk1;
+};
+
+typedef std::map<EMsg, MsgInfo_t *> MsgList;
+typedef std::pair<EMsg, MsgInfo_t *> MsgPair;
+
+MsgList eMsgList;
+
+
+
+CCrypto::CCrypto()
+	: Encrypt_Detour( NULL ), Decrypt_Detour( NULL )
+{
+
+	assert( sizeof( MsgInfo_t ) == 56); // god help the padding never change
+
+	CSimpleScan steamClientScan( "steamclient.dll" );
+
+
+	char *pEncrypt = NULL;
+	bool bEncrypt = steamClientScan.FindFunction(
+		"\x55\x8B\xEC\x64\xA1\x00\x00\x00\x00\x6A\xFF\x68\x00\x00\x00\x00\x50\x64\x89\x25\x00\x00\x00\x00\xA1\x00\x00\x00\x00"
+		"\x8B\x08\x81\xEC\x00\x00\x00\x00\x85\xC9\x75\x08\x89\x4D\xEC\x88\x4D\xF0\xEB\x27\xE8\x00\x00\x00\x00\x8D\x55\xEC\x52"
+		"\x6A\x04\x6A\x00\x68\x00\x00\x00\x00\x6A\x00\x68\x00\x00\x00\x00\x8B\xC8\x89\x45\xEC\xFF\x15\x00\x00\x00\x00\x88\x45"
+		"\xF0\x53\x56\x57\x83\x7D\x08\x00",
+		"xxxxx????xxx????xxxx????x????xxxx????xxxxxxxxxxxxx????xxxxxxxxx????xxx????xxxxxxx????xxxxxxxxxx",
+		(void **)&pEncrypt
+	);
+
+	Encrypt_Orig = (bool (__cdecl *)(const uint8*, uint32, const uint8*, uint32, uint8*, uint32*, const uint8*, uint32))( pEncrypt );
+
+	g_pLogger->LogConsole( "CCrypto::SymmetricEncrypt = 0x%x\n", Encrypt_Orig );
+
+
+	char *pDecrypt = NULL;
+	bool bDecrypt = steamClientScan.FindFunction(
+		"\x55\x8b\xec\x81\xec\x00\x00\x00\x00\x83\x7d\x08\x00\x53\x56\x8b\x35\x00\x00\x00\x00\x57\x75\x00\x6a\x00\x68\x00\x00"
+		"\x00\x00\x68\x00\x00\x00\x00\x6a\x00\x68\x00\x00\x00\x00\xff\xd6\x83\xc4\x14\x83\x7d\x0c\x00",
+		"xxxxx????xxxxxxxx????xx?xxx????x????xxx????xxxxxxxxx",
+		(void **)&pDecrypt
+	);
+
+	Decrypt_Orig = (bool (__cdecl *)(const uint8*, uint32, uint8*, uint32*, const uint8*, uint32))( pDecrypt );
+
+	g_pLogger->LogConsole( "CCrypto::SymmetricDecrypt = 0x%x\n", Decrypt_Orig );
+
+
+	char *pGetMessageList = NULL;
+	steamClientScan.FindFunction(
+		"\xB8\x00\x00\x00\x00\x84\x05\x00\x00\x00\x00\x75\x47",
+		"x????xx????xx",
+		(void **)&pGetMessageList
+	);
+
+	MsgInfo_t *pInfos = *(MsgInfo_t **)( pGetMessageList + 40 );
+	MsgInfo_t *pEndInfos = *(MsgInfo_t **)( pGetMessageList + 64 );
+	uint16 numMessages = ( ( int )pEndInfos - ( int )pInfos ) / sizeof( MsgInfo_t );
+
+	g_pLogger->LogConsole( "pGetMessageList = 0x%x\npInfos = 0x%x\nnumMessages = %d\n", pGetMessageList, pInfos, numMessages );
+
+
+	for ( uint16 x = 0 ; x < numMessages; x++ )
+	{
+		eMsgList.insert( MsgPair( pInfos->eMsg, pInfos ) );
+
+		pInfos++;
+	}
+
+	if ( eMsgList.size() != 0 )
+	{
+		// should only delete our existing files if we have something new to dump
+		g_pLogger->DeleteFile( "emsg_list.txt", false );
+		g_pLogger->DeleteFile( "emsg_list_detailed.txt", false );
+
+		for ( MsgList::iterator iter = eMsgList.begin() ; iter != eMsgList.end() ; iter++ )
+		{
+			MsgInfo_t *pInfo = iter->second;
+
+			g_pLogger->LogFile( "emsg_list.txt", false, "\t%s = %d,\r\n", pInfo->pchMsgName, pInfo->eMsg );
+			g_pLogger->LogFile( "emsg_list_detailed.txt", false, "\t%s = %d, // flags: %d, server type: %d\r\n", pInfo->pchMsgName, pInfo->eMsg, pInfo->nFlags, pInfo->k_EServerTarget );
+		}
+
+		g_pLogger->LogConsole( "Dumped emsg list! (%d messages)\n", eMsgList.size() );
+	}
+	else
+	{
+		g_pLogger->LogConsole( "Unable to dump emsg list: No messages! (Offset changed?)\n" );
+	}
+
+
+	static bool (__cdecl *encrypt)(const uint8*, uint32, const uint8*, uint32, uint8*, uint32*, const uint8*, uint32) = &CCrypto::SymmetricEncrypt;
+	static bool (__cdecl *decrypt)(const uint8*, uint32, uint8*, uint32*, const uint8*, uint32) = &CCrypto::SymmetricDecrypt;
+
+	if ( bEncrypt )
+	{
+		Encrypt_Detour = new CSimpleDetour((void **) &Encrypt_Orig, *(void**) &encrypt);
+		Encrypt_Detour->Attach();
+
+		g_pLogger->LogConsole( "Detoured SymmetricEncrypt!\n" );
+	}
+	else
+	{
+		g_pLogger->LogConsole( "Unable to hook SymmetricEncrypt: Func scan failed.\n" );
+	}
+
+	if ( bDecrypt )
+	{
+		Decrypt_Detour = new CSimpleDetour((void **) &Decrypt_Orig, *(void**) &decrypt);
+		Decrypt_Detour->Attach();
+
+		g_pLogger->LogConsole( "Detoured SymmetricDecrypt!\n" );
+	}
+	else
+	{
+		g_pLogger->LogConsole( "Unable to hook SymmetricDecrypt: Func scan failed.\n" );
+	}
+}
+
+CCrypto::~CCrypto()
+{
+
+	eMsgList.clear();
+
+	if ( Encrypt_Detour )
+	{
+		Encrypt_Detour->Detach();
+		delete Encrypt_Detour;
+	}
+
+	if ( Decrypt_Detour )
+	{
+		Decrypt_Detour->Detach();
+		delete Decrypt_Detour;
+	}
+}
+
+
+
+bool __cdecl CCrypto::SymmetricEncrypt( const uint8 *pubPlaintextData, uint32 cubPlaintextData, const uint8 *pIV, uint32 cubIV, uint8 *pubEncryptedData, uint32 *pcubEncryptedData, const uint8 *pubKey, uint32 cubKey )
+{
+	g_pLogger->LogNetMessage( k_eNetOutgoing, (uint8 *)pubPlaintextData, cubPlaintextData );
+
+	return (*Encrypt_Orig)( pubPlaintextData, cubPlaintextData, pIV, cubIV, pubEncryptedData, pcubEncryptedData, pubKey, cubKey );
+}
+
+bool __cdecl CCrypto::SymmetricDecrypt( const uint8 *pubEncryptedData, uint32 cubEncryptedData, uint8 *pubPlaintextData, uint32 *pcubPlaintextData, const uint8 *pubKey, uint32 cubKey )
+{
+	bool ret = (*Decrypt_Orig)(pubEncryptedData, cubEncryptedData, pubPlaintextData, pcubPlaintextData, pubKey, cubKey);
+
+	g_pLogger->LogNetMessage( k_eNetIncoming, pubPlaintextData, *pcubPlaintextData );
+
+	return ret;
+}
+
+
+const char* CCrypto::GetMessage( EMsg eMsg, uint8 serverType )
+{
+	for ( MsgList::iterator iter = eMsgList.begin() ; iter != eMsgList.end() ; iter++ )
+	{
+		if ( iter->first == eMsg )
+		{
+			return iter->second->pchMsgName;
+		}
+	}
+
+	return NULL;
+}
---
+++ trunk/SteamWebAPI/SyncSteam/SyncSteam/Form2.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace SyncSteam
+{
+    partial class frmInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFriendList = new System.Windows.Forms.Label();
+            this.lstFriendList = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblFriendList
+            // 
+            this.lblFriendList.AutoSize = true;
+            this.lblFriendList.Location = new System.Drawing.Point(12, 9);
+            this.lblFriendList.Name = "lblFriendList";
+            this.lblFriendList.Size = new System.Drawing.Size(57, 12);
+            this.lblFriendList.TabIndex = 0;
+            this.lblFriendList.Text = "친구 목록";
+            // 
+            // lstFriendList
+            // 
+            this.lstFriendList.FormattingEnabled = true;
+            this.lstFriendList.ItemHeight = 12;
+            this.lstFriendList.Location = new System.Drawing.Point(14, 33);
+            this.lstFriendList.Name = "lstFriendList";
+            this.lstFriendList.Size = new System.Drawing.Size(120, 400);
+            this.lstFriendList.TabIndex = 1;
+            // 
+            // frmInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 454);
+            this.Controls.Add(this.lstFriendList);
+            this.Controls.Add(this.lblFriendList);
+            this.Name = "frmInfo";
+            this.Text = "Steam Info";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFriendList;
+        private System.Windows.Forms.ListBox lstFriendList;
+    }
+}
