4cbe06a4c785ae1433fc2dfdf47ac32a77bdc9b1
thks10 <thks10@1507e5fa-ecf2-0410-b220-ea1f1695357d>
2014-05-05 17:30:25 +0000

    -로그인 화면 추가. -쿠키 상태에 따른, 로그인, 로그아웃 화면 변경. -회원가입 아이디들 스팀아이디에서 이메일로 변경. -signup 에서 로그인화면 이동 추가. -enter키로 로그인, 회원가입 추가. -js 이름 변경 및 추가.

---
+++ trunk/web/index.js
@@ -0,0 +1,96 @@
+//init();
+
+// 쿠키 생성
+ function setCookie(cName, cValue, cDay)
+{
+      var expire = new Date();
+      expire.setDate(expire.getDate() + cDay);
+      cookies = cName + '=' + escape(cValue) + '; path=/ '; // 한글 깨짐을 막기위해 escape(cValue)를 합니다.
+      if(typeof cDay != 'undefined') cookies += ';expires=' + expire.toGMTString() + ';';
+      document.cookie = cookies;
+ }
+
+ // 쿠키 가져오기
+ function getCookie(cName)
+{
+      cName = cName + '=';
+      var cookieData = document.cookie;
+      var start = cookieData.indexOf(cName);
+      var cValue = '';
+      if(start != -1){
+           start += cName.length;
+           var end = cookieData.indexOf(';', start);
+           if(end == -1)end = cookieData.length;
+           cValue = cookieData.substring(start, end);
+      }
+      return unescape(cValue);
+ }
+
+function checkLogin()
+{
+    // signup 을 띄울 것인지, dashboard로 가는 버튼을 띄울 것인지
+    var cookie= getCookie("login");
+        
+    var dom1= $("div .jumbotron");
+    var dom2= $("#top3");    
+        
+    if(cookie == "true")    // 로그인 되어있으면...
+    {
+        dom1.append('<p><a class="btn btn-lg btn-success" data-loading-text="Loading..." href="#">Find friends</a></p>');
+        dom2.html("Logout");
+    }else{
+        dom1.append('<p><a class="btn btn-lg btn-success" data-loading-text="Loading..." href="signup.html">Sign up today</a></p>');
+        dom2.html("Login");
+    }
+    // 버튼 누를때 로딩으로 바꾸는 부분 (boostrap)
+    $('a[data-loading-text]')
+    .click(function () {
+    var btn = $(".btn-success")
+    btn.button('loading')
+    setTimeout(function () {
+        btn.button('reset')
+    }, 30000)
+    });
+
+}
+
+function onLogInStat()
+{    
+    var top3= $("#top3");     
+    if(top3.html() == "Login")
+    {
+        alert('login');
+        var url = "http://raimsoft.com/steam/signin.html";    
+        $(location).attr('href',url);
+    }
+    else
+    {
+        setCookie("login", 0, -1);  // 쿠키 삭제.
+        alert('logout');
+        
+        var url = "http://raimsoft.com/steam/";    
+        $(location).attr('href',url);
+    }
+}
+
+
+function init()
+{
+    //alert("init()");
+    //$("#custom1").append('test');
+    //var c= $("div .jumbotron");
+    //alert(c.html());
+    //c.empty();
+    //content.append('<p>test</p>');
+    //content.append("<p><a class=\"btn btn-lg btn-success\" data-loading-text=\"Loading...\" href=\"signup.html\">Sign up today</a></p>");
+    
+//    $('a[data-loading-text]')
+//    .click(function () {
+//        var btn = $(this)
+//        btn.button('loading')
+//        setTimeout(function () {
+//            btn.button('reset')
+//        }, 30000)
+//    });
+        
+}
